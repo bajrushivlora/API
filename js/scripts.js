823c11e5b966470c4313b8e2ca85062164d34dd1
bajrushivlora <bajrushivlora@gmail.com>
2019-05-03 09:39:51 -0400

    lala

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ window.addEventListener('load', ()=> {
    yourname = document.querySelector('.name');
    namedefinition = document.querySelector('.namedefinition');
    sentenceexample = document.querySelector('.example');
+   thumbsup = document.querySelector('.thumbsup');
+   thumbsdown = document.querySelector('.thumbsdown');
    button = document.querySelector('.go');
 
    let entername;
@@ -19,24 +21,32 @@ window.addEventListener('load', ()=> {
          })
          .then(data => {
             console.log(data.list[1]);
+            document.querySelector('.container').style.visibility = "visible";
+            // document.querySelector('.container2').style.visibility = "visible";
 
-            const { word, definition, example } = data.list[1];
+            const { word, definition, example, thumbs_up, thumbs_down } = data.list[1];
 
-               // var text = document.querySelectorAll('.changeblue');
-               // text.forEach(function() {
-               //    this.replace(`(?<=\[).+?(?=\])`, `<span class="maketextblue">${$1}</span>`);
-               // })
-                
+            // ======== INSTRUCTIONS ========
+            // remove open bracket, and replace with <span>
+            // remove the closing bracket, and replace with </span>
+            // use this to replace the brackets /[\[\]']+/g;
+            // this thing works:::::: const definition_trimmed = definition.replace(/[\[\]']+/g,'');
+           
+            
+            // const definition_trimmed = definition.replace(/[\[\]']+/g,'');
+            // const example_trimmed = example.replace(/[\[\]']+/g,'');
+
+            var o = document.getElementById("changetoblue");
+            o.innerHTML = o.innerHTML.replace(/[\[\]']+/g, '<span class="maketextblue">$&</span>');
+               
                yourname.textContent = word;
                namedefinition.textContent = definition;
                sentenceexample.textContent = example;
-               sentenceexample.textContent = example;
+               //thumbsup.textContent = thumbs_up;
+               //thumbsdown.textContent = thumbs_down;
+            })
+         }
 
-
-         });
-      };
-
-
-   entryBox.addEventListener('submit', getName);
+            entryBox.addEventListener('submit', getName);
 });
 
